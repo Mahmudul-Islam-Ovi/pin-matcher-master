18655094e6bc22c32ea3d54d4d25c05bb0d7f3eb
Mahmudul Islam <mahmudulislam14270@gmail.com>
2021-06-03 17:28:35 +0600

    completed

---
+++ index.js
@@ -0,0 +1,55 @@
+function getPin() {
+    const random = Math.random() * 10000;
+    const pin = (random + '').split('.')[0];
+    if (pin.length === 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+// display Generate pin
+function generatePin() {
+    const pinInput = document.getElementById('pin');
+    pinInput.value = getPin();
+}
+
+// handel calculator button 
+const buttonContainer = document.getElementById('digits-container');
+buttonContainer.addEventListener('click',function(event){
+    const digit =(event.target.innerText);
+    if(isNaN(digit)){
+        // Handle Backspace
+        // Handle clear
+        if(digit === 'C'){
+            const typedInput = document.getElementById('typed-pin');
+            typedInput.value = '';
+
+        }
+    }
+    else{
+        const typedInput = document.getElementById('typed-pin');
+        typedInput.value = typedInput.value + digit;
+    }
+})
+
+function verifyPin(){
+    const pin = document.getElementById('pin').value;
+    const typedPin = document.getElementById('typed-pin').value;
+    if(pin === typedPin){
+        displayMatchResult('block','none');
+    }
+    else{
+        displayMatchResult('none','block');
+
+    }
+}
+
+function displayMatchResult(correctStatus,incorrectStatus){
+    const correct = document.getElementById('correct-pin');
+    correct.style.display = correctStatus;
+    const incorrect = document.getElementById('incorrect-pin');
+    incorrect.style.display = incorrectStatus;
+
+}
+
